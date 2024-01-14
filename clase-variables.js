1c7f56032bd814d7829111d0fd133e410eb4bfa2
Pacool84 <flopez_1384@hotmail.com>
2024-01-14 11:39:27 -0600

    [ADD] Clase de variables, anatomia de una variable

---
+++ clase-variables.js
@@ -0,0 +1,20 @@
+let cajaDeAndy = "Woody";
+console.log(cajaDeAndy);
+
+//NO Permitido
+let c = "Woody";
+let cda = "Woody";
+let pcAndy = "Woody";
+
+//Si Permitido
+let primerCajaDeAndy = "Woody";
+let URL = "https://google.com";
+let idDelUsuario = 10;
+
+//Uso de let
+
+let contador = contador + 1;
+
+//Uso de const
+
+const pi = 3.1416;
