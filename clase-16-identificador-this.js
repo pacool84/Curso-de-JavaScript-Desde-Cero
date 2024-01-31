bcff3961d908d1de5481ee73ce10b6621a7e103f
Pacool84 <flopez_1384@hotmail.com>
2024-01-30 21:45:37 -0600

    [FIX] Clase 15 THIS

--- clase-16-identificador-this.js
+++ clase-16-identificador-this.js
@@ -1,34 +1,22 @@
 //This es el entorno de ejecución de una funcion u objeto
 //Esto tambien se conoce como Enlace Implicito - Implicit Binding
 const house = {
-  dogName: "Nagginie",
+  dogName: "Fido",
   dogGreeting: function () {
-    console.log(`Hi, I´m ${this.dogName}`); //Accedemos a propiedades de este mismo objeto
+    console.log(`Hi, I'm ${this.dogName}`); //Accedemos a propiedades de este mismo objeto
   },
 };
-
 house.dogGreeting();
-
 function dogGreeting() {
-  console.log(`Hi, I´m ${this.dogName}`);
+  console.log(`Hi, I'm ${this.dogName}`); //Este this ya no apunta al objeto que hace alusion
 }
-
-//Otro Ejemplo
-
-const house1 = {
-  dogName: "Firulais",
-  dogAge: 3,
-};
-
-function dogGreeting() {
-  console.log(`Hi, I´m ${this.dogName}`); //Este this ya no apunta al objeto que hace alusion
-}
-
-house1.dogGreeting();
 
 //Otro ejemplo enlace explcito - Explicit Binding
-const newHouse = {
-  dogName: "Misha",
-};
-
-dogGreeting.call(newHouse); //El metodo call nos permite vincular el objeto con la funcion, this hace alusion al nvo objeto newHouse
+const newHouse = { dogName: "Coconut" };
+dogGreeting.call(newHouse);
+function newDogGreeting(owner, address) {
+  console.log(`Hi, I'm ${this.dogName} and I live with ${owner} on ${address}`);
+}
+const owner = "Lucy";
+const address = "Avenue 123";
+newDogGreeting.call(newHouse, owner, address); //El metodo call nos permite vincular el objeto con la funcion, this hace alusion al nvo objeto newHouse
