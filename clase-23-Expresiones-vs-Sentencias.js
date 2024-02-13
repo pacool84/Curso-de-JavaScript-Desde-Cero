316a194441fdb7d2d3833142b54c3a767c156b33
Pacool84 <flopez_1384@hotmail.com>
2024-02-12 21:14:23 -0600

    [ADD] Clase 23, Expresiones vs Declaraciones

---
+++ clase-23-Expresiones-vs-Sentencias.js
@@ -0,0 +1,17 @@
+//Expresiones, pedazos de codigo que producen un valor
+"Hola";
+2 + 3;
+6;
+
+//Declaraciones, pedazos de codigo que generan una instruccion para nuestra programa
+
+const numeroEntero = 1; //Declaracion de una variable
+
+//Ejemplos de Declaraciones
+
+function suma() {}
+const suma
+
+//EXPRESION DE FUNCION 
+
+const suma = function() {}
