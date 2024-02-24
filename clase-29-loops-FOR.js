d0b0c725bc4b772d1bf86ab8fd23ced9eeee976f
Pacool84 <flopez_1384@hotmail.com>
2024-02-24 10:28:35 -0600

    [ADD] Clase 29, Ciclo FOR

---
+++ clase-29-loops-FOR.js
@@ -0,0 +1,10 @@
+//For nos ayuda a realizar una iteracion con cada uno de los elementos en una lista
+// La estructura de un for es la siguiente:
+// for(variable, condicion, incremento) {Codigo a ejecutar}
+
+let list = ["eat", "sleep", "code", "repeat"];
+
+for (let i = 0; i < list.length; i++) {
+  console.log(i);
+  console.log(list[i]);
+}
