3636b3e09ed81a18b501bc80b4adca973f4c00b5
Pacool84 <flopez_1384@hotmail.com>
2024-03-03 12:42:20 -0600

    [ADD] Clase 33, Ciclo While

---
+++ clase-33.while.js
@@ -0,0 +1,15 @@
+//El ciclo WHILE se ejcuta siempre y cuando la instruccion sea true
+/* 
+Su estructura es la siguiente 
+
+wihle (condicion) {
+  Codigo a ejecutar
+}
+*/
+
+let contador = 0;
+
+while (contador <= 10) {
+  console.log(contador);
+  contador++;
+}
