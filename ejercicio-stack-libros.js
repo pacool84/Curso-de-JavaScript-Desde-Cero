256fb2452132033c4586cac7a78ec77eb862fcef
Pacool84 <flopez_1384@hotmail.com>
2024-03-04 21:49:55 -0600

    [CHANGE] Colocando otra forma de realizar ejercicio para manejo de stack / arreglos de libros

--- ejercicio-stack-libros.js
+++ ejercicio-stack-libros.js
@@ -17,7 +17,7 @@ const bookStack = [
   "Aprendiendo REACT",
 ];
 
-let optionMenu;
+let optionMenu = 1;
 
 do {
   console.log("\nOpciones:");
@@ -68,3 +68,40 @@ do {
 } while (optionMenu !== 4);
 
 console.log("¡Gracias por utilizar el programa!");
+
+//Otra forma de resolver el ejercicio
+
+let bookCart = [];
+const ADD_TO_CART_ACTION = "addToCart";
+const REMOVE_FROM_CART_ACTION = "removeFromCart";
+const VIEW_CART_ACTION = "viewCart";
+
+function viewCart() {
+  console.log("Current Cart of Books: ", bookCart);
+}
+
+function performCartActions(action, newBook) {
+  switch (action) {
+    case ADD_TO_CART_ACTION:
+      bookCart.push(newBook);
+      break;
+    case REMOVE_FROM_CART_ACTION:
+      if (bookCart.length === 0) {
+        console.log("Cart is empty. No books to remove.");
+      } else {
+        const removedBook = bookCart.pop();
+        console.log(`Removed book from the cart: ${removedBook}`);
+      }
+      break;
+    case VIEW_CART_ACTION:
+      viewCart();
+      break;
+    default:
+      console.log("Invalid action. Please choose a vaid option.");
+  }
+}
+performCartActions(ADD_TO_CART_ACTION, "Think like a monk");
+performCartActions(VIEW_CART_ACTION);
+performCartActions(ADD_TO_CART_ACTION, "Ego is the enemy.");
+performCartActions(VIEW_CART_ACTION);
+performCartActions(REMOVE_FROM_CART_ACTION);
