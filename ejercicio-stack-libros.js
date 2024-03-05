2025850cfd296bdda0e0bafabd77948a225ab58c
Pacool84 <flopez_1384@hotmail.com>
2024-03-04 21:33:31 -0600

    [ADD] Ejercicio manejo de stacks / arreglos de libros

---
+++ ejercicio-stack-libros.js
@@ -0,0 +1,70 @@
+/* 
+Manejando un stack de libros.
+
+Crear un programa que simule un stack de libros utilizando los metodos push y pop.
+El stack debe de almacenar una coleccion de libros.
+EL usuario debe de poder realizar las siguientes acciones:
+-Agregar un nuevo libro en el TOP del stack.
+-Remover un libro en el TOP del stack.
+-Mostrar el stack actual de los libros.
+
+Implementa un loop que le permita al usuario interactuar con el stack hasta que elija la opción 
+para salir del programa.
+*/
+const bookStack = [
+  "El Caliz de Fuego",
+  "El señor de los Anillos - Las dos Torres",
+  "Aprendiendo REACT",
+];
+
+let optionMenu;
+
+do {
+  console.log("\nOpciones:");
+  console.log("1. Agregar un nuevo libro al Stack");
+  console.log("2. Remover un libro del Stack");
+  console.log("3. Visualizar el Stack");
+  console.log("4. Salir del programa");
+
+  optionMenu = parseInt(
+    prompt("Ingrese el número de la opción que desea realizar:")
+  );
+
+  switch (optionMenu) {
+    case 1:
+      const addBook = prompt("Ingrese el nombre del nuevo libro:");
+      bookStack.push(addBook);
+      console.log(`${addBook} fue agregado al TOP del Stack.`);
+      break;
+
+    case 2:
+      if (bookStack.length === 0) {
+        console.log("El stack de libros está vacío.");
+      } else {
+        const removedBook = bookStack.pop();
+        console.log(`${removedBook} fue removido del Stack.`);
+      }
+      break;
+
+    case 3:
+      if (bookStack.length === 0) {
+        console.log("El stack de libros está vacío.");
+      } else {
+        console.log("Stack actual de libros:");
+        bookStack.forEach((book, index) => {
+          console.log(`${index + 1}. ${book}`);
+        });
+      }
+      break;
+
+    case 4:
+      console.log("Saliendo del programa...");
+      break;
+
+    default:
+      console.log("Opción no válida. Por favor, ingrese un número válido.");
+      break;
+  }
+} while (optionMenu !== 4);
+
+console.log("¡Gracias por utilizar el programa!");
