0f5f0d19b8ca7f70a1016416ecb2b2ff5cb452b8
Pacool84 <flopez_1384@hotmail.com>
2024-03-31 19:44:23 -0600

    [ADD] Complemento al ejercicio financiero

--- ejercicio-financiero.js
+++ ejercicio-financiero.js
@@ -1,12 +1,11 @@
-/* Análisis de transacciones
+/* 
 1. Imagina que tienes una lista de transacciones financieras y deseas realizar varias operaciones de procesamiento de datos. Usa las siguientes instrucciones como guía para completar el ejercicio:
 2. Calcular el Saldo Total: - Utiliza el método reduce para calcular y mostrar el saldo total de todas las transacciones.
 3. Encontrar la Transacción más Grande (Ingreso o Egreso): - Emplea el método reduce para encontrar la transacción con el mayor monto (ya sea ingreso o egreso) y muéstrala en la consola.
 4. Filtrar Transacciones de Compra: - Usa el método filter para obtener y mostrar en la consola solo las transacciones de compra (con montos negativos).
 5. Encontrar una Transacción por Descripción: - Utiliza el método find para buscar y mostrar en la consola una transacción específica por su descripción.
 6. Encontrar el Índice de una Transacción por Monto: - Emplea el método findIndex para encontrar y mostrar en la consola el índice de una transacción específica por su monto.
-7. Actualizar Descripciones de Transacciones: - Utiliza el método forEach para actualizar las descripciones de las transacciones. Añade el prefijo "Gasto: " a las transacciones con montos negativos y "Ingreso: " a las transacciones con montos positivos. Muestra las transacciones actualizadas en la consola.
-Recuerda adaptar y combinar estas operaciones según sea necesario. */
+7. Actualizar Descripciones de Transacciones: - Utiliza el método forEach para actualizar las descripciones de las transacciones. Añade el prefijo "Gasto: " a las transacciones con montos negativos y "Ingreso: " a las transacciones con montos positivos. Muestra las transacciones actualizadas en la consola. */
 
 const transactions = [
   { id: 1, description: "Regalo Cumpleaños", ammount: -40 },
@@ -44,3 +43,31 @@ const purchaseAmmounts = transactions.filter(
 );
 
 console.log("Lo gastos indentificados son: ", purchaseAmmounts);
+
+// Buscar transaccion por descripcion
+
+const searchValue = "Pagos de Escuelas";
+const specificTransaction = transactions.find(
+  (transaction) => transaction.description === searchValue
+);
+
+console.log("La transaccion especifica es: ", specificTransaction);
+
+//Encontrar el indice de una transaccion por monto
+
+const indexTransactionWithAmmount = transactions.findIndex(
+  (transaction) => transaction.ammount === 200
+);
+console.log("El indice de la transaccion es: ", indexTransactionWithAmmount);
+
+//Actualizar descripcion de una transaccion
+
+transactions.forEach((transaction) => {
+  if (transaction.ammount < 0) {
+    transaction.description = `Expense: ${transaction.description}`;
+  } else {
+    transaction.description = `Income: ${transaction.description}`;
+  }
+});
+
+console.log("Transacciones Actualizadas:", transactions);
