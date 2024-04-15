a70fbc5d21da3e22a16db21f002f02f897b98910
Pacool84 <flopez_1384@hotmail.com>
2024-04-14 18:59:40 -0600

    [CHANGE] Clase 50, terminando la clase de objetos

--- clase-50-objetos.js
+++ clase-50-objetos.js
@@ -15,7 +15,41 @@ const carro = {
   modelo: 2023,
   color: "Rojo",
   kilometraje: 30000,
+  serie: {
+    motor: 89374,
+    pintura: 982837482932840,
+  },
   venta() {
     console.log("El modelo de este auto es:", carro.modelo);
   },
 };
+
+console.log(carro);
+
+//Consumiendo info del objeto
+console.log(carro.marca);
+
+//Consumiendo el METODO de un objeto
+console.log(carro.venta());
+
+//Modificando un objeto, agregando propiedad
+carro.placas = "3L6T89";
+console.log(carro);
+
+//Agregando un METODO
+carro.cierreVenta = () => {
+  carro.precio = 3000;
+};
+
+console.log(carro.cierreVenta());
+console.log(carro);
+
+//Accediendo a los objetos anidados dentro de objetos
+
+console.log(`El numero de serie del motor es ${carro.serie.motor}`);
+
+//Eliminar propiedades del objeto
+delete carro.serie.pintura;
+console.log(carro);
+
+//No es posible borrar metodos dentro de un objeto
